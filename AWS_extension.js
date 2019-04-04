055f5f3c52662ec88f1563e13d636e3374d9e11e
Aswin Kumar <aswinkumar2019@gmail.com>
2019-04-04 13:47:54 +0530

    Made some changes

--- AWS_extension.js
+++ AWS_extension.js
@@ -159,7 +159,7 @@ new (function() {
                        }
 		rekognition.compareFaces(params, function(err, data) {
                 if (err) console.log(err, err.stack); // an error occurred
-                else     console.log(data);           // successful response
+                else     prompt(data);           // successful response
 	                 }
                         };
  
