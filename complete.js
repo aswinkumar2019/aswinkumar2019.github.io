c2881d604ba25260721b408ad60530fe1b0513d9
Aswin Kumar <aswinkumar2019@gmail.com>
2019-04-29 12:44:28 +0530

    Added alert box

--- complete.js
+++ complete.js
@@ -182,7 +182,9 @@ new(function () {
 		rekognition.compareFaces(comparams, function (err, data) {
 			if (err) console.log(err, err.stack); // an error occurred
 			else console.log(data); // successful response
-			alert(data[FaceMatches].FaceMatches[0].[similarity]);
+			for (var i=0; i<data.length; i++) {
+                              Alert(" \n Similarity : " + i + data[0][i].Similarity);
+			}
 		});
 	};
 
