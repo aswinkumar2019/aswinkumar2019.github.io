cdf598896c5fd699e98ffd38d8396376a03e5d5f
Aswin Kumar <aswinkumar2019@gmail.com>
2019-05-06 11:48:17 +0530

    Added scratch blocks

--- Google_scratch.js
+++ Google_scratch.js
@@ -67,3 +67,35 @@ function recognizeSpeech(audioData, recognizeInputLanguage, recognitionHandler) 
 				recognitionHandler(text);
 		});
 	}
+
+        var descriptor = {
+		blocks: [
+			[' ', 'initialise %s', 'initgoogle'],
+
+			['-'],
+			['-'],
+
+			[' ', 'choose language %m.languages', 'setLanguage', 'English'],
+			['w', 'say %s', 'speak', 'Hello Kids'],
+
+			['-'],
+			['-'],
+
+			[' ', 'choose source language %m.sourceLanguages', 'setSourceLanguage', 'English'],
+			[' ', 'choose target language %m.targetLanguages', 'setTargetLanguage', 'Chinese'],
+			[' ', 'Choose Voice %m.voices', 'setvoice', 'Lucia'],
+			['w', 'translate %s', 'translate', 'Hello'],
+			['r', 'translatedText', 'getTranslatedText']
+
+		],
+		menus: {
+			languages: ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese'],
+		        voices: ['Lucia', 'Joanna', 'Penelope', 'Filiz', 'Lea', 'Vicki', 'Carla'],
+			sourceLanguages: ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese'],
+			targetLanguages: ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese']
+		},
+	};
+
+	ScratchExtensions.register('Youcode Intelligence Solutions (Preparing KIDS for AI future)', descriptor, ext);
+	})();
+
