3ba5b0d339ff36807783465a8033527fb10997ed
Aswin Kumar <aswinkumar2019@gmail.com>
2019-05-16 14:58:13 +0530

    Update google_video.js

--- google_video.js
+++ google_video.js
@@ -98,6 +98,7 @@ new(function () {
 	
 	ext.videooutput = function () {
 		var url = prompt("Enter the video url");
+		var labeltype = prompt("Enter the label Detection type,values maybe SHOT_MODE,FRAME_MODE and SHOT_AND_FRAME_MODE");
 		var output = prompt("Enter the bucket link where you want the output");
 		gapi.client.videos.annotate(
 			{
@@ -105,6 +106,9 @@ new(function () {
                         "features": [
                                  videotype
                                     ],
+			"videoContext": {
+                              "labelDetectionMode": labeltype,
+                                       },
                           "outputUri": output,
                           }).then(function(r) {
 			prompt("Inside result block");
