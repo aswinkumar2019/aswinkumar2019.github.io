63d01b3a600e72a381a6af8f26890e226579ac47
Aswin Kumar <aswinkumar2019@gmail.com>
2019-05-17 09:49:29 +0530

    Update google_complete.js

--- google_complete.js
+++ google_complete.js
@@ -75,12 +75,7 @@ new(function () {
                                   },
                          "audioConfig": {
                                 "audioEncoding": "MP3",
-                                "speakingRate": 1.0,
-                                "pitch": 0.0,
-                                "volumeGainDb": 0.0,
-                               // "sampleRateHertz": number,
-                                "effectsProfileId": [
-                                              "medium-bluetooth-speaker-class-device"
+                               // "sampleRateHertz": number
                                              ]
                                   }
 		}).then(function(r) {
