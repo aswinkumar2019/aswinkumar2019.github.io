39a9b035cea43b1595c8a6c9fed5ccf6f5e653f6
Aswin Kumar <aswinkumar2019@gmail.com>
2019-05-17 10:48:57 +0530

    Update google_complete.js

--- google_complete.js
+++ google_complete.js
@@ -82,7 +82,7 @@ new(function () {
 			console.log(r.result.audioContent);
 			var enc = window.atob(r.result.audioContent);
 			console.log(enc);
-			var uInt8Array = new Uint8Array(enc.AudioStream);
+			var uInt8Array = new Uint8Array(enc);
 			var arrayBuffer = uInt8Array.buffer;
 			var blob = new Blob([arrayBuffer]);
 		        var url = URL.createObjectURL(blob);
