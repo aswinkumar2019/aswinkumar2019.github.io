ae7b1f2b6993e744b4ec907e11ea84a244e747ff
Aswin Kumar <aswinkumar2019@gmail.com>
2019-05-31 11:31:08 +0530

    Update microsoft_scratch.js

--- microsoft_scratch.js
+++ microsoft_scratch.js
@@ -1,1 +1,38 @@
+new(function () {
+
+var ext = this;
 $.getScript('https://aswinkumar2019.github.io/microsoft.cognitiveservices.speech.sdk.bundle.js', initExtension);
+
+var descriptor = {
+		blocks: [
+			[' ', 'initialise %s', 'initAzure'],
+
+			['-'],
+			['-'],
+
+			[' ', 'choose language %m.languages', 'setLanguage', 'English'],
+			['w', 'say %s', 'speak', 'Hello KIDS'],
+
+			['-'],
+			['-'],
+
+			[' ', 'choose source language %m.sourceLanguages', 'setSourceLanguage', 'English'],
+			[' ', 'choose target language %m.targetLanguages', 'setTargetLanguage', 'Chinese'],
+			[' ', 'Choose Voice %m.voices', 'setvoice', 'Lucia'],
+			[' ', 'Comparefaces', 'comparebucket'],
+			[' ', 'Detect Faces', 'detectfaces'],
+			[' ', 'DetectText', 'detectit'],
+			['w', 'translate %s', 'translate', 'Hello'],
+			['r', 'translatedText', 'getTranslatedText']
+
+		],
+		menus: {
+			languages: ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese'],
+		        voices: ['Lucia', 'Joanna', 'Penelope', 'Filiz', 'Lea', 'Vicki', 'Carla'],
+			sourceLanguages: ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese'],
+			targetLanguages: ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese']
+		},
+	};
+
+	ScratchExtensions.register('Youcode Intelligence Solutions (Preparing KIDS for AI future)', descriptor, ext);
+})();
